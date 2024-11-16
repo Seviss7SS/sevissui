dd02e0eae853cca92da0a852e4caf45188038842
Terence Brewer <terencedbrewer@gmail.com>
2024-11-16 11:59:21 -0700

    increased exports, added change event to textarea

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,29 @@
 // @ts-nocheck
 
 // components
+import Breadcrumbs from "$lib/components/Breadcrumbs/Breadcrumbs.svelte";
 import Button from "$lib/components/Button/Button.svelte";
 import Card from "$lib/components/Card/Card.svelte";
-import Progress from "$lib/components/Progress/Progress.svelte";
-import Breadcrumbs from "$lib/components/Breadcrumbs/Breadcrumbs.svelte";
+import Checkbox from "$lib/components/Checkbox/Checkbox.svelte";
+import Dropdown from "$lib/components/Dropdown/Dropdown.svelte";
+import FormRenderer from "$lib/components/FormRenderer/FormRenderer.svelte";
 import InputField from "$lib/components/InputField/InputField.svelte";
+import Modal from "$lib/components/Modal/Modal.svelte";
+import Pagination from "$lib/components/Pagination/Pagination.svelte";
+import Progress from "$lib/components/Progress/Progress.svelte";
 import Textarea from "$lib/components/Textarea/Textarea.svelte";
 
-export { Button, Breadcrumbs, Card, Progress, InputField, Textarea };
+export {
+  Button,
+  Breadcrumbs,
+  Card,
+  Checkbox,
+  Dropdown,
+  FormRenderer,
+  Progress,
+  InputField,
+  Modal,
+  Pagination,
+  Progress,
+  Textarea,
+};
